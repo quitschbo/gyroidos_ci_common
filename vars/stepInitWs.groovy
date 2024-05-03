c6b78d11c2844a1325facc9870c819de109f7f6c
Johannes Wiesböck <johannes.wiesboeck@aisec.fraunhofer.de>
2024-05-03 13:55:30 +0200

    vars/stepInitWs: Parse pull requests from target.pr_branches
    Signed-off-by: Johannes Wiesböck <johannes.wiesboeck@aisec.fraunhofer.de>

--- vars/stepInitWs.groovy
+++ vars/stepInitWs.groovy
@@ -8,6 +8,8 @@ def call(Map target = [:]) {
 	// selector: Build selector for CopyArtifact step
 	// rebuild_previous: Specifies whether sources should be built again
 	// 					 when running pipeline on a previous build
+	// pr_branches: Comma separated list of pull requests for specific repos that
+	//	should override the default branch
 
 
 	echo "Running on host: ${NODE_NAME}"
@@ -39,51 +41,53 @@ def call(Map target = [:]) {
 	"""
 
 	// TODO test port with CML PRs
-	sh label: 'Parse PRs + repo sync', script: '''
+	sh label: 'Parse PRs + repo sync', script: """
+		mkdir -p .repo/local_manifests
+
 		meta_repos="meta-trustx|meta-trustx-intel|meta-trustx-rpi|meta-trustx-nxp"
 		cml_repo="cml"
 		build_repo="gyroidos_build"
 		branch_regex="PR-([0-9]+)"
 
-		echo $branches | tr ',' '\n' | while read -r line; do
-			if [[ "$line" =~ ($meta_repos)=$branch_regex ]]; then
-				project="${BASH_REMATCH[1]}"
-				revision="refs/pull/${BASH_REMATCH[2]}/head"
+		echo "${target.pr_branches}" | tr ',' '\n' | while read -r line; do
+			if [[ "\$line" =~ (\$meta_repos)=\$branch_regex ]]; then
+				project="\${BASH_REMATCH[1]}"
+				revision="refs/pull/\${BASH_REMATCH[2]}/head"
 
 				echo "\
 <?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\n\
 <manifest>\n\
-<remove-project name=\\\"$project\\\" />\n\
-<project path=\\\"$project\\\" name=\\\"$project\\\" remote=\\\"gyroidos\\\" revision=\\\"$revision\\\" />\n\
-</manifest>" >> .repo/local_manifests/$project.xml
-			elif [[ "$line" =~ ($cml_repo)=$branch_regex ]]; then
-				project="${BASH_REMATCH[1]}"
-				revision="refs/pull/${BASH_REMATCH[2]}/head"
+<remove-project name=\\\"\$project\\\" />\n\
+<project path=\\\"\$project\\\" name=\\\"\$project\\\" remote=\\\"gyroidos\\\" revision=\\\"\$revision\\\" />\n\
+</manifest>" >> .repo/local_manifests/\$project.xml
+			elif [[ "\$line" =~ (\$cml_repo)=\$branch_regex ]]; then
+				project="\${BASH_REMATCH[1]}"
+				revision="refs/pull/\${BASH_REMATCH[2]}/head"
 
 				echo "\
 <?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\n\
 <manifest>\n\
-<remove-project name=\\\"$project\\\" />\n\
-<project path=\\\"trustme/cml\\\" name=\\\"$project\\\" remote=\\\"gyroidos\\\" revision=\\\"$revision\\\" />\n\
-</manifest>" >> .repo/local_manifests/$project.xml
-			elif [[ "$line" =~ ($build_repo)=$branch_regex ]]; then
-				project="${BASH_REMATCH[1]}"
-				revision="refs/pull/${BASH_REMATCH[2]}/head"
+<remove-project name=\\\"\$project\\\" />\n\
+<project path=\\\"trustme/cml\\\" name=\\\"\$project\\\" remote=\\\"gyroidos\\\" revision=\\\"\$revision\\\" />\n\
+</manifest>" >> .repo/local_manifests/\$project.xml
+			elif [[ "\$line" =~ (\$build_repo)=\$branch_regex ]]; then
+				project="\${BASH_REMATCH[1]}"
+				revision="refs/pull/\${BASH_REMATCH[2]}/head"
 
 				echo "\
 <?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\n\
 <manifest>\n\
-<remove-project name=\\\"$project\\\" />\n\
-<project path=\\\"trustme/build\\\" name=\\\"$project\\\" remote=\\\"gyroidos\\\" revision=\\\"$revision\\\" />\n\
-</manifest>" >> .repo/local_manifests/$project.xml
+<remove-project name=\\\"\$project\\\" />\n\
+<project path=\\\"trustme/build\\\" name=\\\"\$project\\\" remote=\\\"gyroidos\\\" revision=\\\"\$revision\\\" />\n\
+</manifest>" >> .repo/local_manifests/\$project.xml
 			else
-				echo "Could not parse revision for line $line"
+				echo "Could not parse revision for line \$line"
 			fi
 		done
 
 		repo sync -j8 --current-branch --fail-fast
 
-	'''
+	"""
 
 	sh "tar -C \"${target.workspace}\" -cf \"${target.workspace}/sources-${target.gyroid_arch}-${target.gyroid_machine}.tar\" --exclude=sources-${target.gyroid_arch}-${target.gyroid_machine}.tar ."
 
