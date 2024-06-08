92b395a42792ef84a60edeba11eed2f1ed270159
Felix Wruck <felix.wruck@aisec.fraunhofer.de>
2024-06-08 08:13:51 +0200

    vars/stepSyncMirrors: Increase SSH verbosity
    This commit adds the -v option to ssh connection in stepSyncMirrors.
    
    Signed-off-by: Felix Wruck <felix.wruck@aisec.fraunhofer.de>

--- vars/stepSyncMirrors.groovy
+++ vars/stepSyncMirrors.groovy
@@ -19,17 +19,17 @@ def call(Map target = [:]) {
 			ATTIC="\$MIRRORPATH/attic_sstate/${target.buildtype}/\$(TZ=UTC date +%Y%m%d_%H%M%S)_${target.buildtype}"
 			TARPATH="\$ATTIC/sstate_${target.buildtype}.tar"
 
-			rsync -v -e "ssh -l jenkins-ssh" -r --ignore-existing --no-devices --no-specials --no-links "${target.workspace}/out-${target.buildtype}/downloads/" ${env.MIRRORHOST}:"\$SOURCES"
+			rsync -v -e "ssh -v -l jenkins-ssh" -r --ignore-existing --no-devices --no-specials --no-links "${target.workspace}/out-${target.buildtype}/downloads/" ${env.MIRRORHOST}:"\$SOURCES"
 
-			ssh jenkins-ssh@${env.MIRRORHOST} "mkdir \$ATTIC"
+			ssh -v jenkins-ssh@${env.MIRRORHOST} "mkdir \$ATTIC"
 
-			ssh jenkins-ssh@${env.MIRRORHOST} "find \$SSTATE -mindepth 1 -maxdepth 1 -exec mv '{}' \$ATTIC \\;"
+			ssh -v jenkins-ssh@${env.MIRRORHOST} "find \$SSTATE -mindepth 1 -maxdepth 1 -exec mv '{}' \$ATTIC \\;"
 
 			tar -C "${target.workspace}/out-${target.buildtype}/sstate-cache/" -cf "${target.workspace}/sstate_${target.buildtype}.tar" .
 
-			rsync -v -e "ssh -l jenkins-ssh" "sstate_${target.buildtype}.tar" ${env.MIRRORHOST}:"\$TARPATH"
+			rsync -v -e "ssh -v -l jenkins-ssh" "sstate_${target.buildtype}.tar" ${env.MIRRORHOST}:"\$TARPATH"
 
-			ssh jenkins-ssh@${env.MIRRORHOST} "cd \$SSTATE && tar -xf \$TARPATH"
+			ssh -v jenkins-ssh@${env.MIRRORHOST} "cd \$SSTATE && tar -xf \$TARPATH"
 		"""
 	}
 }
